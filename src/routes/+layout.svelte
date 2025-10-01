6a490d23bdc240795bdc28268237952cbe563562
mlodybercik <przemekbarcicki@gmail.com>
2025-10-01 16:24:14 +0200

    About me mostly done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { page } from '$app/state';
+
 	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
 
@@ -9,4 +11,28 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="flex min-h-screen w-full justify-center">
+	<div class="flex w-95 flex-col md:w-xl lg:w-4xl">
+		<header>
+			<div class="flex justify-center">
+				<nav class="py-2">
+					<a class="px-2" href="/" aria-current={page.url.pathname === '/'}>whoami</a>
+					<a class="px-2" href="/projects" aria-current={page.url.pathname === '/projects'}>Projects</a>
+					<a class="px-2" href="/hire" aria-current={page.url.pathname === '/hire'}>Hire me</a>
+				</nav>
+			</div>
+		</header>
+		<main class="grow">
+			{@render children?.()}
+		</main>
+		<footer class="grey-100 mt-1 text-xs font-thin">
+			Seen an issue? Submit it <a href="https://github.com/mlodybercik/dev.bercik.xyz">here</a>
+		</footer>
+	</div>
+</div>
+
+<style>
+	nav > a[aria-current='true'] {
+		text-decoration: underline;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,119 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const uptime = new Date(new Date().valueOf() - new Date(2000, 11, 27).valueOf());
+	const years = uptime.getFullYear() - 1970;
+	const months = uptime.getMonth() ? `${uptime.getMonth()} months` : '';
+
+	const stripes = [
+		// first row
+		'bg-gray-900',
+		'bg-red-500',
+		'bg-green-500',
+		'bg-yellow-500',
+		'bg-blue-500',
+		'bg-purple-500',
+		'bg-teal-500',
+		'bg-gray-200',
+		'bg-gray-200', // dummy
+
+		// second row
+		'bg-gray-800',
+		'bg-red-400',
+		'bg-green-400',
+		'bg-yellow-400',
+		'bg-blue-400',
+		'bg-purple-400',
+		'bg-teal-400',
+		'bg-gray-100'
+	];
+</script>
+
+{#snippet block(color: string)}
+	<div class="h-4 w-8 {color} float-left"></div>
+{/snippet}
+
+<div class="flex h-full flex-col items-center justify-center py-5">
+	<div>
+		<p class="pb-3" aria-hidden="true">
+			<span class="g">dev.bercik.xyz@froggy</span> <span class="b">~ $</span> neofetch
+		</p>
+		<div class="flex flex-col lg:flex-row">
+			<pre role="img" class="icon pb-5">
+            .-/+oo####oo+/-.
+        `:+##################+:`
+      -+########################+-
+    .o############################o.
+   /################################/
+  +##################################+
+ /####################################/
+.######################################.
++######################################+
+o######################################o
+o######################################o
++######################################+
+.######################################.
+ /####################################/
+  +##################################+
+   /################################/
+    .o############################o.
+      -+########################+-
+        `:+##################+:`
+            .-/+oo####oo+/-.</pre>
+			<div class="ml-5 flex flex-col items-center justify-center">
+				<ul>
+					<li>Przemysław Barcicki</li>
+					<li>------------</li>
+					<li><span class="r">Country:</span> <span class="font-bold">Poland</span></li>
+					<li><span class="r">City:</span> <span class="font-bold">Wrocław</span></li>
+					<li><span class="r">Uptime:</span> <span class="font-bold">{years} years, {months}</span></li>
+					<li><span class="r">Trade:</span> <span class="font-bold">Information Technology</span></li>
+					<li><span class="r">Specialization:</span> <span class="font-bold">Web Applications</span></li>
+					<li><span class="r">Education:</span> <span class="font-bold">MSc's, BSc's, Technical</span></li>
+					<li><span class="r">Languages:</span> <span class="font-bold">Python, TypeScript, Java</span></li>
+					<li><span class="r">Dream job:</span> <span class="font-bold">Backend developer</span></li>
+					<!-- TODO: change this -->
+					<li><span class="r">Status:</span> <span class="font-bold">Unemployed</span></li>
+				</ul>
+				<div class="w-full pt-2">
+					<p>
+						{#each stripes as color}
+							{@render block(color)}
+						{/each}
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- TODO: scale font to always fit width -->
+<style>
+	pre {
+		line-height: normal;
+		font-weight: 900;
+		user-select: none;
+	}
+
+	.g {
+		color: var(--color-green-500);
+	}
+
+	.r {
+		color: var(--color-red-400);
+	}
+
+	.b {
+		color: var(--color-blue-500);
+	}
+
+	p > div:nth-child(9) {
+		float: none;
+	}
+
+	.icon {
+		background: url('https://avatars.githubusercontent.com/u/42300497?s=512&v=4') no-repeat;
+		background-clip: text;
+		background-size: cover;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+</style>
